2c7f34d2c7394e1eab922d9ce63ec171816a4b61
Ayşe Akışık <ayseakisik16@gmail.com>
2022-05-22 15:39:14 +0300

    controllers created

---
+++ NewsSearchWebApi/NewsSearchWebApi/Controllers/KeywordController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsSearchWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewsSearchWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeywordController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public KeywordController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Keyword> GetAllKeywords()
+        {
+            var list = _context.Keywords.ToList();
+            return list;
+        }
+
+        [HttpPost]
+        public void CreateKeyword(Keyword item)
+        {
+            var x = GetAllKeywords().Where(x => x.Word == item.Word).ToList();
+
+            if (x.Count == 0)
+            {
+                _context.Keywords.Add(item);
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteKeyword(int id)
+        {
+            var deletedKeyword = _context.Keywords.Where(x => x.Id == id).FirstOrDefault();
+            _context.Keywords.Remove(deletedKeyword);
+            _context.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateKeyword(int id)
+        {
+            var updatedKeyword = _context.Keywords.Where(x => x.Id == id).FirstOrDefault();
+            _context.Keywords.Update(updatedKeyword);
+            _context.SaveChanges();
+        }
+
+        [HttpGet("{id}")]
+        public Keyword GetKeywordById(int id)
+        {
+            var getItem = _context.Keywords.Where(x => x.Id == id).FirstOrDefault();
+            return getItem;
+        }
+    }
+}
---
+++ NewsSearchWebApi/NewsSearchWebApi/Controllers/NewsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsSearchWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewsSearchWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public NewsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public void CreateNews(News item)
+        {
+            var x = GetAllNews().Where(x => x.Url == item.Url).ToList();
+
+            if (x.Count == 0)
+            {
+                var lastItem = GetAllNews().LastOrDefault();
+                if (lastItem.PublishedAt < item.PublishedAt)
+                {
+                    _context.News.Add(item);
+                    _context.SaveChanges();
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteNews(int id)
+        {
+            var deletedNew = _context.News.Where(x => x.Id == id).FirstOrDefault();
+            _context.News.Remove(deletedNew);
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        public List<News> GetAllNews()
+        {
+            var list = _context.News.ToList();
+            return list;
+        }
+
+        [HttpGet("{id}")]
+        public News GetNewsById(int id)
+        {
+            var getItem = _context.News.Where(x => x.Id == id).FirstOrDefault();
+            return getItem;
+        }
+    }
+}
